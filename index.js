52914d73ec6d209586d8a4785e1c8a9f1251d834
Zoran Todorovic <zoran@arsfutura.co>
2017-01-17 11:12:05 +0100

    Port env var added.

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-// var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
-server.listen(8080, function () {
+server.listen(port, function () {
 	// transports: ['xhr-polling']
-	transports: ['websocket']
-  console.log('Server listening at port 8080');
+	// transports: ['websocket']
+  console.log('Server listening at port 3000');
 });
 
 // Routing
